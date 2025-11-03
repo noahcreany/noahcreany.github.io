728b81e7c7a5bc90b4458c68a3b1a48ae5fd209b
noahcreany <86799936+noahcreany@users.noreply.github.com>
2025-11-03 16:20:33 -0700

    Update scholar_citations.py

--- scholar_citations.py
+++ scholar_citations.py
@@ -2,16 +2,21 @@ from parsel import Selector
 import requests, os, json
 import pandas as pd
 import altair as alt
+import time
+import random
 
 def parsel_scrape_author_cited_by_graph():
-    params = {'user': 'yWVPrskAAAAJ',
-              'hl': 'en'} 
+    time.sleep(random.uniform(5, 10)) # Wait between 5 and 10 seconds
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'
-    }
+    params = {'user': 'yWVPrskAAAAJ','hl': 'en'} 
 
-    html = requests.get('https://scholar.google.com/citations', params=params, headers=headers, timeout=100)
+    USER_AGENT_LIST = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
+                       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15',
+                       'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:127.0) Gecko/20100101 Firefox/127.0']
+
+    headers = {'User-Agent': random.choice(USER_AGENT_LIST)}
+
+    html = requests.get('https://scholar.google.com/citations', params=params, headers=headers, timeout=50)
     selector = Selector(text=html.text)
 
     data = {
